ffc5aa0b713cbabeb959e2b962846bbd6c1b6131
Rajesh282000 <rajeshburnwal28112000@gmail.com>
2025-06-11 22:11:17 +0530

    added config file of webclient in order-service

--- order-service/src/main/java/com/firewall/order_service/service/OrderService.java
+++ order-service/src/main/java/com/firewall/order_service/service/OrderService.java
@@ -1,20 +1,28 @@
 package com.firewall.order_service.service;
 
+import com.firewall.order_service.config.WebClientConfig;
 import com.firewall.order_service.dto.OrderDto;
 import com.firewall.order_service.model.Order;
 import com.firewall.order_service.model.OrderLineItems;
 import com.firewall.order_service.repository.OrderRepo;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
+@Transactional
 public class OrderService {
     private final OrderRepo orderRepo;
 
-    public OrderService(OrderRepo orderRepo) {
+    private final WebClient webClient;
+
+
+    public OrderService(OrderRepo orderRepo, WebClient webClient) {
         this.orderRepo = orderRepo;
+        this.webClient = webClient;
     }
 
     public void placeOrder(OrderDto orderDto) {
@@ -31,6 +39,12 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItemsList);
 
+        //call inventory service, and place order if product is in stock
+        webClient.get()
+                .uri("http://localhost:8082/api/inventory")
+                 .retrieve()
+                .bodyToMono(Boolean.class)
+                .block();
         orderRepo.save(order);
     }
 }
